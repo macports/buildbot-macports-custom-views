9a8167d7d45b7413d0d957337345f71eb282e7b4
Rajdeep Bharati <rajdeepbharati13@gmail.com>
2019-06-06 16:18:31 +0530

    Add MIT License

--- setup.py
+++ setup.py
@@ -1,19 +1,4 @@
 #!/usr/bin/env python
-#
-# This file is part of Buildbot.  Buildbot is free software: you can
-# redistribute it and/or modify it under the terms of the GNU General Public
-# License as published by the Free Software Foundation, version 2.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-# details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this program; if not, write to the Free Software Foundation, Inc., 51
-# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
-# Copyright Buildbot Team Members
 
 from __future__ import absolute_import
 from __future__ import division
@@ -32,7 +17,6 @@ setup_www_plugin(
     author=u'Rajdeep Bharati',
     author_email=u'rajdeepbharati13@gmail.com',
     url='https://buildbot.net/',
-    license='GNU GPL',
     packages=['buildbot_macports_custom_views'],
     package_data={
         '': [
@@ -44,4 +28,7 @@ setup_www_plugin(
         [buildbot.www]
         buildbot_macports_custom_views = buildbot_macports_custom_views:ep
     """,
+    classifiers=[
+        'License :: OSI Approved :: MIT License'
+    ],
 )
