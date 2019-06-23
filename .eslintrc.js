36c41b88226e80b2e64b256c1019033e2cced7f7
Rajdeep Bharati <rajdeepbharati13@gmail.com>
2019-06-23 19:46:08 +0530

    Set up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    'plugin:vue/recommended'
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+    semi: [2, 'never'],
+    quotes: ['error', 'single'],
+    indent: ['error', 2, { SwitchCase: 1 }]
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  }
+}
