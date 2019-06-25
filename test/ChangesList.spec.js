6d844a720c4ef4c6b7a0936f6918f33565988e98
Rajdeep Bharati <rajdeepbharati13@gmail.com>
2019-06-25 19:32:43 +0530

    Add basic unit tests

--- test/ChangesList.spec.js
+++ test/ChangesList.spec.js
@@ -1,7 +1,15 @@
 import ChangesList from '@/components/ChangesList.vue'
 
 describe('ChangesList', () => {
-  it('sets data', () => {
+  it('sets the correct default data', () => {
     expect(typeof ChangesList.data).toBe('function')
+    const defaultData = ChangesList.data()
+    expect(defaultData.showChange).toBe(false)
+    expect(defaultData.selectedChange).toEqual({})
+    expect(defaultData.buttonTxt).toBe('Show changes after this date')
+    expect(defaultData.before).toBe(true)
+    expect(typeof defaultData.rdate).toBe('number')
+    expect(defaultData.loading).toBe(true)
+    expect(defaultData.errored).toBe(false)
   })
 })
---
+++ test/FilterBuilds.spec.js
@@ -0,0 +1,11 @@
+import FilterBuilds from '@/components/FilterBuilds.vue'
+
+describe('FilterBuilds', () => {
+  it('sets the correct default data', () => {
+    expect(typeof FilterBuilds.data).toBe('function')
+    const defaultData = FilterBuilds.data()
+    expect(defaultData.buttonTxt).toBe('Show builds after this date')
+    expect(defaultData.before).toBe(true)
+    expect(typeof defaultData.rdate).toBe('number')
+  })
+})
