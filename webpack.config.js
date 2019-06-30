eab7b07c214eadeea12666395ab73e376434ebe7
Rajdeep Bharati <rajdeepbharati13@gmail.com>
2019-06-30 11:46:21 +0530

    Disable sourcemaps (#18)

--- webpack.config.js
+++ webpack.config.js
@@ -48,9 +48,6 @@ module.exports = {
     })
   ],
 
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: 'source-map',
-
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: ['.ts', '.tsx', '.js', '.json', '.vue'],
