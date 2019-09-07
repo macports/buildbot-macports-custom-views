afc7a4ef5494db7b4e7e9234ed58c00def0fc645
Rajdeep Bharati <rajdeep@macports.org>
2019-09-07 20:31:26 +0530

    Allow live updates in Ports Build History view

--- src/ports.directive.js
+++ src/ports.directive.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import PortsList from './components/PortsList.vue'
 
+import axios from 'axios'
+
 angular
   .module('buildbot_macports_custom_views')
   .config([
@@ -54,30 +56,64 @@ angular
       const settings = bbSettingsService.getSettingsGroup('PortsBuildsHistory')
 
       function link(scope, element, attrs) {
-        /* create an instance of the data accessor */
-        var dataAccessor = dataService.open().closeOnDestroy(scope)
-        console.log('dataccessor', dataAccessor)
+        // create an instance of the data accessor
+        const dataAccessor = dataService.open().closeOnDestroy(scope)
 
-        var builders = dataAccessor.getBuilders({
+        const builders = dataAccessor.getBuilders({
           limit: settings.builderLimit.value,
           order: '-builderid'
         })
-        var builds = dataAccessor.getBuilds({
+        const builds = dataAccessor.getBuilds({
           limit: settings.buildLimit.value,
           order: '-started_at',
           property: '*'
         })
-        var workers = dataAccessor.getWorkers({
+        const workers = dataAccessor.getWorkers({
           order: '-workerid'
         })
 
         const pageSize = settings.pageSize.value
 
-        var ComponentClass = Vue.extend(PortsList)
+        const ComponentClass = Vue.extend(PortsList)
 
-        /* cannot pass the changes directly, as the magic of buildbot 
-          data module clashes with the magic of vue observers */
-        var data = {
+        /**
+         * TODO: Make data module compatible with Vue, so that collections
+         * can be directly passed to Vue SFC. Right now we are creating new arrays
+         * for different properties (builds, builders, etc), and appending items
+         * to them and passing it as data to the component.
+         */
+        let buildsArray = [],
+          buildersArray = [],
+          workersArray = []
+
+        builds.onNew = build => {
+          axios
+            .get(`${window.location.origin}/api/v2/builds/${build.buildid}`)
+            .then(response => {
+              buildsArray.push(response.data.builds[0])
+            })
+          data.builds = buildsArray
+        }
+        builders.onNew = builder => {
+          axios
+            .get(
+              `${window.location.origin}/api/v2/builders/${builder.builderid}`
+            )
+            .then(response => {
+              buildersArray.push(response.data.builders[0])
+            })
+          data.builders = buildersArray
+        }
+        workers.onNew = worker => {
+          axios
+            .get(`${window.location.origin}/api/v2/workers/${worker.workerid}`)
+            .then(response => {
+              workersArray.push(response.data.workers[0])
+            })
+          data.workers = workersArray
+        }
+
+        let data = {
           location: $location,
           builders: [],
           builds: [],
@@ -85,25 +121,12 @@ angular
           pageSize: pageSize
         }
 
-        var e = new ComponentClass({
+        new ComponentClass({
           data: data,
           el: element.get(0)
         })
 
-        function update() {
-          data.scope = scope
-          data.location = $location
-          data.builders = builders.slice()
-          data.builds = builds.slice()
-          data.workers = workers.slice()
-          scope.$digest()
-        }
-
-        scope.onChange = () => update()
-        $location.onChange = () => update()
-        builders.onChange = () => update()
-        builds.onChange = () => update()
-        workers.onChange = () => update()
+        data.scope = scope
       }
 
       return {
